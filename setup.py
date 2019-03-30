750a413a0e1c19614666f8f52387673680b89d12
Michal Charemza <michal@charemza.name>
2019-03-30 05:41:41 +0000

    (release) v0.0.12

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='aiothrottler',
-    version='0.0.11',
+    version='0.0.12',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='Throttler for asyncio Python',
